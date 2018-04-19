7f32ecb1494eb81932e2ce1fabb379de40f92357
Jason Rice <ricejasonf@gmail.com>
2018-04-19 09:35:26 -0700

    Add yaml_lock test

---
+++ test/yaml_lock/run_test.py
@@ -0,0 +1,4 @@
+# TODO
+# call `cppdock init` in this working dir
+# test that yaml file is the same except for specifying revisions in each dep
+# The dep with the hardcoded revision should NOT change
